eddacd8116e277976997e5c6cca30905e813295b
Ulises Miranda <ulimirandachavez@hotmail.com>
2023-06-17 23:15:55 -0600

    amazon

--- src/app/services/promedio.service.ts
+++ src/app/services/promedio.service.ts
@@ -6,7 +6,9 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
   providedIn: 'root'
 })
 export class PromedioService {
-  url_py ='http://localhost:5000/'
+  //url_py ='http://localhost:5000/'
+  //url_aws ='https://kr0un06gxd.execute-api.us-east-2.amazonaws.com/'
+  url_aws ='https://duozqzwij5.execute-api.us-east-2.amazonaws.com/dev'
   constructor(
     private http:HttpClient
   ) {
@@ -14,16 +16,19 @@ export class PromedioService {
 
   obtenerPromedio(lista: string[]){
     const headers = new HttpHeaders({
-      'Access-Control-Allow-Origin': 'http://localhost:4200'
+
     });
     const options = { headers: headers };
 
-    const myObject: { [index: number]: string } = {};
+    const myObject: { [index: string]: string } = {};
 
     lista.forEach((value, index) => {
-      myObject[index] = value;
+      myObject[index.toString()] = value;
     });
     console.log(myObject)
-    return this.http.post(this.url_py+'obtener-promedio',myObject,options);
+    console.log(myObject)
+    //return this.http.post(this.url_py+'obtener-promedio',myObject,options);
+    //return this.http.post(this.url_aws+'obtener-promedio',myObject,options);
+    return this.http.post(this.url_aws,myObject,options);
   }
 }
