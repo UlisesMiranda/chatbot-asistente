3c67b684e5c1a6a1e74cbee10c0537eeb30967bc
Ulises Miranda <ulimirandachavez@hotmail.com>
2023-06-19 22:34:51 -0600

    localhost

--- src/app/services/rasa.service.ts
+++ src/app/services/rasa.service.ts
@@ -7,8 +7,8 @@ import {JSONFile} from "@angular/cli/src/utilities/json-file";
   providedIn: 'root'
 })
 export class RasaService {
-  //private apiUrl = 'http://localhost:5005/webhooks/rest/webhook';
-  private apiUrl = 'https://8d06-2806-104e-18-22b0-a5d8-b715-63b3-b7f.ngrok-free.app/webhooks/rest/webhook';
+  private apiUrl = 'http://localhost:5005/webhooks/rest/webhook';
+  //private apiUrl = 'https://8d06-2806-104e-18-22b0-a5d8-b715-63b3-b7f.ngrok-free.app/webhooks/rest/webhook';
   token = "bp_pat_b8eBdrNqGlPdb8K4NCGvsb1AObQKnwEhKHzD"
 
   constructor(
